92d3a607fa57de2c124eb61f234cdc27976dffb8
BeastLe9enD <beastle9end@outlook.de>
2022-09-16 13:20:59 +0200

    ✨ Add bird

---
+++ sanctuary-in-the-woods/Assets/Scripts/Objects/Animals/BirdStorage.cs
@@ -0,0 +1,22 @@
+﻿using Animals;
+using Animals.Birds;
+using Inventory;
+using UnityEngine;
+
+namespace Objects.Animals
+{
+    public sealed class BirdStorage : MonoBehaviour
+    {
+        void Start()
+        {
+            var itemRegistry = FindObjectOfType<ItemRegistry>();
+
+            var stateManager = GetComponent<AnimalStateManager>();
+            stateManager.AddState(new AnimalIdleState(new StackedItem(itemRegistry.WeedFeed)));
+            stateManager.AddState(new AnimalTamedState(new StackedItem(itemRegistry.WeedFeed), typeof(BirdTradeState)));
+            stateManager.AddState(new BirdTradeState());
+            
+            stateManager.Switch<AnimalIdleState>();
+        }
+    }
+}
