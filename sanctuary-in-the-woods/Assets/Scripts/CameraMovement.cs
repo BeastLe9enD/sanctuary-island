bb9863db34540c9c0c1b83bbc7fe48d99a95c898
zmarlon <marlon.klaus@icloud.com>
2022-09-16 23:36:59 +0200

    ✨ Chaning camera scale with + and -

--- sanctuary-in-the-woods/Assets/Scripts/CameraMovement.cs
+++ sanctuary-in-the-woods/Assets/Scripts/CameraMovement.cs
@@ -7,4 +7,26 @@ public sealed class CameraMovement : MonoBehaviour
     void FixedUpdate() {
         transform.position = new Vector3(FollowTransform.position.x, FollowTransform.position.y, transform.position.z);
     }
+
+    private void Update() {
+        var camera = Camera.main;
+        
+        if(Input.GetKey(KeyCode.Plus))
+        {
+            camera.orthographicSize += 3 * Time.deltaTime;
+            if(camera.orthographicSize > 10)
+            {
+                camera.orthographicSize = 10;
+            }
+        }
+ 
+ 
+        if(Input.GetKey(KeyCode.Minus))
+        {
+            camera.orthographicSize -= 3 * Time.deltaTime;
+            if(camera.orthographicSize < 4) {
+                camera.orthographicSize = 4;
+            }
+        }
+    }
 }
