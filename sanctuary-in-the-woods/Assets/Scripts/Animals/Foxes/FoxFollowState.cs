2eab2824fb37e6436d20c5b53e144f4bbe885969
BeastLe9enD <beastle9end@outlook.de>
2022-09-17 20:18:04 +0200

    🚧 Sync

--- sanctuary-in-the-woods/Assets/Scripts/Animals/Foxes/FoxFollowState.cs
+++ sanctuary-in-the-woods/Assets/Scripts/Animals/Foxes/FoxFollowState.cs
@@ -1,7 +1,19 @@
-﻿namespace Animals.Foxes
+﻿using UnityEngine;
+
+namespace Animals.Foxes
 {
-    public class FoxFollowState : IAnimalState
+    public sealed class FoxFollowState : IAnimalState
     {
+        private Player _player;
         
+        public void OnEnter(AnimalStateManager manager)
+        {
+            _player = Object.FindObjectOfType<Player>();
+        }
+
+        public void OnFixedUpdate(AnimalStateManager manager)
+        {
+            manager.Agent.SetDestination(_player.transform.position);
+        }
     }
 }
