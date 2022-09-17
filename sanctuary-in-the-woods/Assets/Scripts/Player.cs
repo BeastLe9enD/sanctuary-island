035bd61300c2788453e1a5f6405f5e2a980fd44c
Luisa Gerharz <gerharzl@hochschule-trier.de>
2022-09-17 23:16:43 +0200

    ✨ added Main Menu as Pause Menu (Esc)

--- sanctuary-in-the-woods/Assets/Scripts/Player.cs
+++ sanctuary-in-the-woods/Assets/Scripts/Player.cs
@@ -26,9 +26,10 @@ public class Player : MonoBehaviour
     private GameObject _gameObject;
     
     #region PREFABS
-
+    
     public GameObject BirdHouse;
     public GameObject BerryPlant;
+    public GameObject MainMenuCanvas;
     #endregion
 
     private void Start()
@@ -85,11 +86,6 @@ public class Player : MonoBehaviour
         TopTilemap.SetTile(tile_pos, null);
     }
 
-    private IEnumerator PetAnimal() {
-        if (!Input.GetMouseButtonDown(0)) yield break;
-        // TODO: add animation (3 loops of Interact), behaviour of animal
-    }
-    
     // Update is called once per frame
     void Update() {
         Movement();
@@ -114,7 +110,7 @@ public class Player : MonoBehaviour
         }
 
         if (Input.GetKeyDown(KeyCode.Escape)) {
-            // TODO: PAUSE/UNPAUSE
+            MainMenuCanvas.SetActive(!MainMenuCanvas.activeSelf);
         }
     }
 
--- sanctuary-in-the-woods/Assets/Scripts/UI/MainMenu.cs
+++ sanctuary-in-the-woods/Assets/Scripts/UI/MainMenu.cs
@@ -6,68 +6,45 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
     public GameObject pauseMenu, optionsMenu, creditsMenu;
-    
+
     public void Play() {
         SceneManager.LoadScene("World");
+    }
+    
+    public void Resume() {
+        gameObject.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
     }
 
     public void QuitGame() {
         Application.Quit();
     }
-    
-    public void OpenOptions()
-    {
+
+    public void OpenOptions() {
         optionsMenu.SetActive(true);
         pauseMenu.SetActive(false);
-    
+
         EventSystem.current.SetSelectedGameObject(null);
     }
-    
-    public void CloseOptions()
-    {
+
+    public void CloseOptions() {
         optionsMenu.SetActive(false);
         pauseMenu.SetActive(true);
-    
+
         EventSystem.current.SetSelectedGameObject(null);
     }
-    
-    public void OpenCredits()
-    {
+
+    public void OpenCredits() {
         creditsMenu.SetActive(true);
         pauseMenu.SetActive(false);
 
         EventSystem.current.SetSelectedGameObject(null);
     }
 
-    public void CloseCredits()
-    {
+    public void CloseCredits() {
         creditsMenu.SetActive(false);
         pauseMenu.SetActive(true);
 
         EventSystem.current.SetSelectedGameObject(null);
-    }
-
-    public void PauseUnpause() {
-        // if (!pauseMenu.activeInHierarchy)
-        // {
-        //     pauseMenu.SetActive(true);
-        //     Time.timeScale = 0f;
-        //     EventSystem.current.SetSelectedGameObject(null);
-        //     EventSystem.current.SetSelectedGameObject(pauseFirstButton);
-        // }
-        // else
-        // {
-        //     pauseMenu.SetActive(false);
-        //     optionsMenu.SetActive(false);
-        //     creditsMenu.SetActive(false);
-        //     StartCoroutine(WaitTimeScale());
-        // }
-    }
-    
-    private IEnumerator WaitTimeScale()
-    {
-        yield return new WaitForSecondsRealtime(0.2f);
-        Time.timeScale = 1f;
-        //GameObject.FindWithTag("Player").GetComponent<PlayerMovement>().paused = false;
     }
 }
